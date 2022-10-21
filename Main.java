6cbe89e33230cd1dc7da9249e2c639fc29b1e3bf
Ryosuke Oya <ryoll329555@docomo.ne.jp>
2022-10-21 14:43:34 +0900

    fixed anya age

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     // main method,
     public static void main(String[] args) {
         User.saySleepy();
-        User anya = new User("Anya",8);
+        User anya = new User("Anya",6);
         anya.introduceSelf();
         anya.setName("AnyaForger");
         // can not anya.name, because name is private field
